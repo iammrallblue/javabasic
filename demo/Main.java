5a043dd11b1789fff9332b34904fe793375286e3
joh <iammrallblue@gmail.com>
2021-03-27 10:32:59 -0400

    demo

--- demo/Main.java
+++ demo/Main.java
@@ -1,19 +1,22 @@
 package demo;
 
 public class Main {
+  /** main method. */
   public static void main(String[] args) {
     Hash h = new Hash();
     h.add(1);
     h.add(2);
     int x = h.remove(3);
-    if (x == -1)
+    if (x == -1) {
       System.out.println("Hey value does not exist at all");
+
+    }
+
     boolean t = h.contains(2);
-
-    if (t == false)
+    if (t == false) {
       System.out.println("Hey nothing there");
-    else
+    } else {
       System.out.println("Hey value is there");
-
+    }
   }
 }
